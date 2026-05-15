407593c1e56bdc8d43f6f6063e4856a3f07fd88e
bugrabalim11 <bugrablm11@gmail.com>
2026-05-15 11:48:24 +0300

    feat: Announcements (Son duyurular) ViewComponent entegrasyonu ile dinamik hale getirildi.

---
+++ AgriculturePresentation/ViewComponents/_AnnouncementPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstarct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _AnnouncementPartial : ViewComponent
+    {
+        private readonly IAnnouncementService _announcementService;
+
+        public _AnnouncementPartial(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _announcementService.GetListAll();
+            return View(values);
+        }
+    }
+}
