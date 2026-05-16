15834de0e8e28ea3ee31278cbf2e51baf8618324
bugrabalim11 <bugrablm11@gmail.com>
2026-05-16 12:43:47 +0300

    feat: Admin giriş ekranı teması, veritabanı modeli ve yönetim işlemleri eklendi.

---
+++ BusinessLayer/Abstract/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLayer.Abstarct;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAdminService : IGenericService<Admin>
+    {
+    }
+}
---
+++ EntityLayer/Concrete/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    public class Admin
+    {
+        public int AdminID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
