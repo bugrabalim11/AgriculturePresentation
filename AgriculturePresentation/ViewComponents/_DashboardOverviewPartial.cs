cd76eb285f71beb87626d1bd2179fedc975d55c7
bugrabalim11 <bugrablm11@gmail.com>
2026-05-17 11:52:24 +0300

    feat: Dashboard view component içerisine istatistik kodlarının eklenmesi ve arayüze bağlanması.

--- AgriculturePresentation/ViewComponents/_DashboardOverviewPartial.cs
+++ AgriculturePresentation/ViewComponents/_DashboardOverviewPartial.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.ViewComponents
 {
     public class _DashboardOverviewPartial : ViewComponent
     {
+        AgricultureContext c = new AgricultureContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.teamCount = c.Teams.Count();
+            ViewBag.serviceCount = c.Services.Count();
+            ViewBag.messageCount = c.Contacts.Count();
+            ViewBag.currentMonthMessageCount = c.Contacts.Where(x => x.Date.Month == DateTime.Now.Month && x.Date.Year == DateTime.Now.Year).Count();
+
+            ViewBag.announcementTrue = c.Announcements.Where(x => x.Status == true).Count();
+            ViewBag.announcementFalse = c.Announcements.Where(x => x.Status == false).Count();
+
+            ViewBag.urunPazarlama = c.Teams.Where(x => x.Title == "Ürün Pazarlama").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.bakliyatYönetimi = c.Teams.Where(x => x.Title == "Bakliyat Üreticisi").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.sutUreticisi = c.Teams.Where(x => x.Title == "Süt Üreticisi").Select(y => y.PersonName).FirstOrDefault();
+            ViewBag.gubreYonetimi = c.Teams.Where(x => x.Title == "Gübre Yönetimi").Select(y => y.PersonName).FirstOrDefault();
             return View();
         }
     }
