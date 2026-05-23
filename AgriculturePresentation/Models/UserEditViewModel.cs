5c8251a12b08a5e9e8a73f2c955284a8fde2444d
bugrabalim11 <bugrablm11@gmail.com>
2026-05-23 16:39:11 +0300

    feat: Profil işlemleri için ProfileController oluşturuldu ve temel yapısı kuruldu. feat: Profil bilgileri güncelleme özelliği eklendi (Update metotları aktif edildi. feat: Dependency Injection yapısı Container katmanı ile modüler hale getirildi.

---
+++ AgriculturePresentation/Models/UserEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AgriculturePresentation.Models
+{
+    public class UserEditViewModel
+    {
+        public string mail { get; set; }
+        public string phone { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+    }
+}
--- AgriculturePresentation/Program.cs
+++ AgriculturePresentation/Program.cs
@@ -1,6 +1,7 @@
 using BusinessLayer.Abstarct;
 using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
+using BusinessLayer.Container;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete.EntityFramework;
 using DataAccessLayer.Contexts;
@@ -15,32 +16,12 @@ var builder = WebApplication.CreateBuilder(args);
 // EPPlus 8 ve üstü sürümler için yeni Lisans tanımlama kuralı
 ExcelPackage.License.SetNonCommercialPersonal("Buğra");
 
-// Add services to the container.
-builder.Services.AddScoped<IServiceService, ServiceManager>();
-builder.Services.AddScoped<IServiceDal, EfServiceDal>();
-builder.Services.AddScoped<ITeamService, TeamManager>();
-builder.Services.AddScoped<ITeamDal, EfTeamDal>();
-builder.Services.AddScoped<IAnnouncementService, AnnouncementManager>();
-builder.Services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
-builder.Services.AddScoped<IImageService, ImageManager>();
-builder.Services.AddScoped<IImageDal, EfImageDal>();
-builder.Services.AddScoped<IAddressService, AddressManager>();
-builder.Services.AddScoped<IAddressDal, EfAddressDal>();
-builder.Services.AddScoped<IContactService, ContactManager>();
-builder.Services.AddScoped<IContactDal, EfContactDal>();
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
-builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
-builder.Services.AddScoped<IAboutService, AboutManager>();
-builder.Services.AddScoped<IAboutDal, EfAboutDal>();
-builder.Services.AddScoped<IAdminService, AdminManager>();
-builder.Services.AddScoped<IAdminDal, EfAdminDal>();
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
-
 
 builder.Services.AddDbContext<AgricultureContext>();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AgricultureContext>();
+
+builder.Services.ContainerDependencies();
 
 builder.Services.AddControllersWithViews();
 
